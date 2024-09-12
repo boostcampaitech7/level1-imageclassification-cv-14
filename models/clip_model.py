6999e071f84a124d484c8f28768245751920b933
오홍석 <lkl4502@kookmin.ac.kr>
2024-09-13 04:03:43 +0900

    feat(model) : clip_model 파일 생성

--- models/clip_model.py
+++ models/clip_model.py
@@ -1,5 +1,14 @@
 import torch.nn as nn
+from transformers import CLIPModel
 
-class CLIPModel(nn.Module):
-    def __init__(self):
-        super(CLIPModel, self).__init__()
+class ClipCutomModel(nn.Module):
+    def __init__(self,
+                 model_name: str):
+        super(ClipCutomModel, self).__init__()
+        self.model = CLIPModel.from_pretrained(model_name)
+    
+    def forward(self, x):
+        # with torch.no_grad():
+        #     features = self.model.encode_image(x).float()  # Convert to float32
+        # return self.classifier(features)
+        return self.model(x)
